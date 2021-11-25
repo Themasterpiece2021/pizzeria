9115760064feb98c16b008ba7886a0eaeda30a6a
“NyronFlackters” <cruzsuazocristianandres@gmail.com>
2021-11-25 12:11:03 -0500

    Se realiza el middleware de admin y se agrega role a model user

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  roleId: { type: mongoose.Schema.ObjectId, ref: "roles" },
   registerDate: { type: Date, default: Date.now },
   dbstatus: { type: Boolean, default: true},
 });
